ad4ada5b6be203f14be2589c15c571a8c9efc932
webgently <gently039@gmail.com>
2025-05-10 08:01:18 +0900

    feat: add NotFound component with 404 error page template

---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center justify-center py-16 font-primary">
+    <img src="/images/404.png" alt="404 Not Found" class="mb-8" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+};
+</script>
