557856a783bb05a10349931bdfc6e8a97c43b18a
webgently <gently039@gmail.com>
2025-05-10 08:02:09 +0900

    feat: refactor App component to include AppHeader and AppFooter, replacing welcome message with router view

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="welcomeMessage">
-    Hi there 👋, <br /><br />      
-    Welcome to your test task. <br /><br />
-    Before you begin make sure to read the README file from the repository to make sure that your environment is properly set up. <br /><br />
-    Also please make sure to read the challenge instructions carefully. We advice that you push your source code to the repository frequently to avoid any loss of work.<br />
-    Once you are ready to submit the work, just go back to the challenge instructions page and click the "Submit Challenge" button.<br /><br />      
-    Good Luck and Have Fun! 🤞<br /><br /><br /><br />
-    <em><strong>NOTE:</strong> This page is only a welcome message and you should overwrite this page with the actuall solution implementation.</em>
+  <div class="min-h-screen flex flex-col font-primary">
+    <AppHeader />
+
+    <main class="flex-grow container max-w-[90%] mx-auto px-4 pt-[60px] pb-[20px]">
+      <router-view></router-view>
+    </main>
+
+    <AppFooter />
   </div>
 </template>
 
-<style>
-@import "./index.css";
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
 
-.welcomeMessage {
-  margin: 120px auto;
-  width: 50%;
-  text-align: left;
-  font-size: 24px;
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter
+  }
 }
+</script>
+
+<style>
+@import './index.css';
 </style>
