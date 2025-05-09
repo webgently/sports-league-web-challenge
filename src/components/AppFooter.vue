5f7fa2ae159cf26ab4a35e1d265bf4f2a7092e51
webgently <gently039@gmail.com>
2025-05-10 08:00:31 +0900

    feat: add AppFooter component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="flex items-center bg-footer-bg h-[40px] font-primary">
+    <div class="container mx-auto pr-[40px] text-right text-footer-text">API Version: 1.0</div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+};
+</script>
